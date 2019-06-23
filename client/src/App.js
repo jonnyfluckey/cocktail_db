b60c97d83a6b3e054eb542bbedb7e6a8829e7d13
Jonny Fluckey <jonathon.fluckey@gmail.com>
2019-06-23 14:23:34 -0600

    added navbar, built out card

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from 'react';
-import axios from 'axios'
+import axios from 'axios';
+import {Card, Image, Icon} from 'semantic-ui-react';
+import Navbar from './Components/Shared/Navbar';
 
 
 
@@ -12,17 +14,55 @@ function App() {
     fetchData();
   }, [])
 
-  const center = {
+  const style = {
+    center: {
     display: 'block',
     marginRight: 'auto',
     marginLeft: 'auto',
+    },
+    background: {
+      backgroundImage: "url('https://images.unsplash.com/photo-1540224769541-7e6e20a42330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80')",
+      height: '100%',
+      width: '100%',
+      position: 'absolute',
+    }
   }
+
+    //  ingredientList( ()=> {
+    //   Object.keys(drinkInfo).map((key, index) => {
+    //     if (drinkInfo[key] != "" && drinkInfo[key].includes("Ingredient") == true) {
+    //       return(
+    //         <>
+    //         <Icon name='chevron right'/>
+    //         <li>{drinkInfo.key}</li>
+    //         </>
+    //       )
+    //     }
+    //   })
+    // })
 
   return (
     <>
-    <h1 style={{textAlign: 'center'}}>Cocktail of the Day</h1>
+    <Navbar/>
+    <div style={style.background}>
+    <h1 style={{textAlign: 'center', color: 'white'}}>Cocktail of the Day</h1>
     <br></br>
-    <img src={drinkInfo.strDrinkThumb} alt="drink" style={center} height='200px' width='200px' />
+    <Card style={style.center}>
+    <Image src={drinkInfo.strDrinkThumb} centered size={"huge"} />
+    <Card.Content>
+      <Card.Header>{drinkInfo.strDrink}</Card.Header>
+      <Card.Meta>
+      <ul>
+   
+      </ul>
+      </Card.Meta>
+    </Card.Content>
+    <Card.Content extra>
+      <Card.Header>Instructions</Card.Header>
+      <Card.Description>{drinkInfo.strInstructions}</Card.Description>
+    </Card.Content>
+    </Card>
+    </div>
     </>
   );
 }
